bfd860c92442689d79b43fef7ebaf8d79b6c43be
Dave Roberts <d.roberts@ctrlo.com>
2026-09-22 13:52:46 +0100

    Revert "Update to use C"
    This reverts commit ad5e6e6a3d905e00bef073eb100ff97c71c501a5.

--- Makefile
+++ Makefile
@@ -1,13 +1,12 @@
-all: boot.asm loader.asm kernel.asm
+all: boot.asm loader.asm
+	@echo "Starting build process"
+	@echo "Building Orion boot image"
 	@nasm -f bin -o boot.bin boot.asm
+	@echo "Building Orion loader"
 	@nasm -f bin -o loader.bin loader.asm
-	@nasm -f elf64 -o kernel.o kernel.asm
-	@gcc -std=c99 -mcmodel=large -ffreestanding -fno-stack-protector -mno-red-zone -c main.c 
-	@ld -nostdlib -T link.lds -o kernel kernel.o main.o
-	@objcopy -O binary kernel kernel.bin 
+	@echo "Writing loader to image"
 	@dd if=boot.bin of=boot.img bs=512 count=1 conv=notrunc
 	@dd if=loader.bin of=boot.img bs=512 count=5 seek=1 conv=notrunc
-	@dd if=kernel.bin of=boot.img bs=512 count=100 seek=6 conv=notrunc
 
 clean:
-	@rm -f boot.bin loader.bin kernel.o main.o kernel kernel.bin
+	@rm -f boot.bin loader.bin
--- boot.asm
+++ boot.asm
@@ -1,184 +1,70 @@
 [BITS 16]
-[ORG 0x7e00]
+[ORG 0x7C00]
 
 start:
-    mov [DriveId],dl ; Store the drive ID passed by the BIOS into the DriveId variable
+    xor ax,ax ; Set all values to 0
+    mov ds,ax
+    mov es,ax
+    mov ss,ax
+    mov sp,0x7C00 ; Set stack pointer to top of boot sector
 
-    mov eax,0x80000000 ; Get the highest extended function supported by CPUID
-    cpuid ; Call CPUID with EAX=0x80000000 to get the highest extended function supported
-    cmp eax,0x80000001 ; Check if the highest extended function supported is at least 0x80000001
-    jb NotSupport ; Jump to NotSupport if the highest extended function is less than 0x80000001
+TestDiskExtension:
+    mov [DriveId], dl ; Store the drive ID for later use
+    mov ah, 0x41 ; Check for extended disk services support
+    mov bx, 0x55aa ; Signature for extended disk services
+    int 0x13
+    jc NotSupported ; Jump if carry flag is set (error)
+    cmp bx, 0xaa55 ; Check if the signature is correct
+    jne NotSupported ; Jump if not supported
 
-    mov eax,0x80000001 ; Get extended feature flags
-    cpuid ; Call CPUID with EAX=0x80000001 to get extended feature flags
-    test edx,(1<<29) ; Check if the CPU supports LM (Long Mode)
-    jz NotSupport ; Jump to NotSupport if LM is not supported
-    test edx,(1<<26) ; Check if the CPU supports NX (No-Execute)
-    jz NotSupport ; Jump to NotSupport if NX is not supported
+LoadLoader:
+    mov si, ReadPacket
+    mov word[si], 0x10 ; Set size of the read packet
+    mov word[si+2], 5 ; Number of sectors to read
+    mov word[si+4], 0x7e00 ; Segment address to load the sectors
+    mov word[si+6], 0 ; Offset address to load the sectors
+    mov dword[si+8], 1 ; Starting LBA sector
+    mov dword[si+0xc], 0 ; Reserved, set to 0
+    mov dl, [DriveId] ; Set the drive ID for the read operation
+    mov ah, 0x42 ; Extended read sectors from drive
+    int 0x13 ; Call BIOS to read sectors
+    jc ReadError ; Jump if there was an error reading sectors
 
-LoadKernel:
-    mov si,ReadPacket ; Load the address of the ReadPacket structure into SI
-    mov word[si],0x10 ; Set the size of the packet structure
-    mov word[si+2],100 ; Set the number of sectors to read
-    mov word[si+4],0 ; Set the segment of the buffer
-    mov word[si+6],0x1000 ; Set the offset of the buffer
-    mov dword[si+8],6 ; Set the drive number
-    mov dword[si+0xc],0 ; Set the reserved field
-    mov dl,[DriveId] ; Load the drive ID into DL for the BIOS interrupt call
-    mov ah,0x42 ; BIOS extended read sectors function
-    int 0x13 ; Call BIOS interrupt to read sectors
-    jc  ReadError ; Jump to ReadError if the carry flag is set
+    mov dl, [DriveId]
+    jmp 0x7e00 ; Jump to the loaded code
 
-GetMemInfoStart:
-    mov eax,0xe820 ; Get memory map using BIOS interrupt 0x15, function 0xE820
-    mov edx,0x534d4150 ; 'SMAP' signature
-    mov ecx,20 ; Size of the buffer for the memory map entry
-    mov edi,0x9000 ; Address of the buffer to store the memory map entry
-    xor ebx,ebx ; Set EBX to 0 for the first call
-    int 0x15 ; Call BIOS interrupt to get memory map
-    jc NotSupport ; Jump to NotSupport if the carry flag is set
+ReadError: ; Label for handling read errors
+NotSupported: ; Label for handling unsupported disk services
+    mov ah,0x13 ; BIOS teletype output function
+    mov al,1 ; Number of characters to write
+    mov bx,0xa ; Page number and attribute
+    xor dx,dx ; Row and column
+    mov bp,Message ; Pointer to the message
+    mov cx,MessageLength ; Length of the message
+    int 0x10 ; Call BIOS to display the message
+    jmp End
 
-GetMemInfo:
-    add edi,20 ; Move to the next memory map entry
-    mov eax,0xe820 ; Get memory map using BIOS interrupt 0x15, function 0xE820
-    mov edx,0x534d4150 ; 'SMAP' signature
-    mov ecx,20 ; Size of the buffer for the memory map entry
-    int 0x15 ; Call BIOS interrupt to get memory map
-    jc GetMemDone ; Jump to GetMemDone if the carry flag is set
-
-    test ebx,ebx ; Check if there are more memory map entries
-    jnz GetMemInfo ; Jump to GetMemInfo if there are more entries
-
-GetMemDone:
-TestA20:
-    mov ax,0xffff ; Load 0xffff into AX for setting ES segment
-    mov es,ax ; Set ES segment to 0xffff
-    mov word[ds:0x7c00],0xa200 ; Write test value to memory
-    cmp word[es:0x7c10],0xa200 ; Compare with the mirrored address in the high memory
-    jne SetA20LineDone ; If not equal, A20 line is already enabled
-    mov word[0x7c00],0xb200 ; Write another test value
-    cmp word[es:0x7c10],0xb200 ; Compare with the mirrored address in the high memory
-    je End ; If equal, jump to End
-    
-
-SetA20LineDone:
-    xor ax,ax ; Clear AX register
-    mov es,ax ; Set ES segment to 0x0000 after clearing AX
-
-SetVideoMode:
-    mov ax,3 ; Set video mode to 80x25 text mode
-    int 0x10 ; Call BIOS interrupt to set video mode
-    
-    cli ; Clear interrupts before entering protected mode
-    lgdt [Gdt32Ptr] ; Load the Global Descriptor Table for 32-bit protected mode
-    lidt [Idt32Ptr] ; Load the Interrupt Descriptor Table for 32-bit protected mode
-
-    mov eax,cr0 ; Load CR0 register
-    or eax,1 ; Set the PE (Protection Enable) bit
-    mov cr0,eax ; Store the updated value back to CR0
-
-    jmp 8:PMEntry ; Far jump to the protected mode entry point
-
-ReadError:
-NotSupport:
 End:
     hlt ; Halt the CPU
     jmp End ; Loop indefinitely after halting the CPU
 
+DriveId db 0 ; Store the drive ID for later use
+Message db 'Error in boot loader' ; Error message to display
+MessageLength equ $ - Message ; Length of the error message
+ReadPacket times 16 db 0 ; Buffer for the extended read packet structure
 
-[BITS 32]
-PMEntry:
-    mov ax,0x10 ; Load the data segment selector for protected mode
-    mov ds,ax ; Set DS segment for protected mode
-    mov es,ax ; Set ES segment for protected mode
-    mov ss,ax ; Set SS segment for protected mode
-    mov esp,0x7c00 ; Set stack pointer for protected mode
+times (0x1be - ($ - $$)) db 0 ; Fill the rest of the boot sector up to the partition table
 
-    cld ; Clear the direction flag for string operations
-    mov edi,0x70000 ; Destination address for clearing memory
-    xor eax,eax ; Clear EAX register to use as the value for stosd
-    mov ecx,0x10000/4 ; Number of double words to clear
-    rep stosd ; Clear memory by storing EAX into the destination repeatedly
-    
-    mov dword[0x70000],0x71007 ; Set up initial memory values
-    mov dword[0x71000],10000111b ; Set up additional memory values
+    db 80h ; Bootable flag for the first partition
+    db 0,2,0 ; CHS address of the first sector
+    db 0f0h ; Partition type
+    db 0ffh,0ffh,0ffh ; CHS address of the last sector
+    dd 1 ; LBA of the first sector
+    dd (20*16*63-1) ; Number of sectors in the partition
 
+    times (16 * 3) db 0 ; Reserved space for additional partition entries
 
-    lgdt [Gdt64Ptr] ; Load the Global Descriptor Table for 64-bit long mode
-
-    mov eax,cr4 ; Load CR4 register
-    or eax,(1<<5) ; Set the PAE (Physical Address Extension) bit
-    mov cr4,eax ; Store the updated value back to CR4
-
-    mov eax,0x70000 ; Load the base address of the page directory
-    mov cr3,eax ; Store the page directory base address into CR3
-
-    mov ecx,0xc0000080 ; Load the MSR address for enabling NX bit
-    rdmsr ; Read the MSR into EDX:EAX
-    or eax,(1<<8) ; Set the NXE (No-Execute Enable) bit
-    wrmsr ; Write the updated value back to the MSR
-
-    mov eax,cr0 ; Load CR0 register
-    or eax,(1<<31) ; Set the PG (Paging) bit
-    mov cr0,eax ; Store the updated value back to CR0
-
-    jmp 8:LMEntry ; Far jump to the 64-bit long mode entry point
-
-PEnd:
-    hlt ; Halt the CPU
-    jmp PEnd ; Loop indefinitely after halting the CPU
-
-[BITS 64]
-LMEntry:
-    mov rsp,0x7c00 ; Set stack pointer for 64-bit long mode
-
-    cld ; Clear the direction flag for string operations
-    mov rdi,0x200000 ; Destination address for memory copy
-    mov rsi,0x10000 ; Source address for memory copy
-    mov rcx,51200/8 ; Number of quad words to copy
-    rep movsq ; Copy memory from source to destination
-
-    jmp 0x200000 ; Jump to the copied code in memory
-    
-LEnd:
-    hlt ; Halt the CPU
-    jmp LEnd ; Loop indefinitely after halting the CPU
-
-DriveId:    db 0 ; Drive ID for the boot drive
-ReadPacket: times 16 db 0 ; Buffer for reading disk packets
-
-Gdt32:
-    dq 0 ; Null descriptor for the GDT
-Code32:
-    dw 0xffff
-    dw 0
-    db 0
-    db 0x9a
-    db 0xcf
-    db 0
-Data32:
-    dw 0xffff
-    dw 0
-    db 0
-    db 0x92
-    db 0xcf
-    db 0
-    
-Gdt32Len: equ $-Gdt32
-
-Gdt32Ptr: dw Gdt32Len-1
-          dd Gdt32
-
-Idt32Ptr: dw 0
-          dd 0
+    db 0x55 ; Boot sector signature
+    db 0xaa ; Boot sector signature
 
 
-Gdt64:
-    dq 0
-    dq 0x0020980000000000
-
-Gdt64Len: equ $-Gdt64
-
-
-Gdt64Ptr: dw Gdt64Len-1
-          dd Gdt64
